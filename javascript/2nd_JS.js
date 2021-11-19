96310c99f7468a82fe6bc5896734b625333d9e58
matv864 <91239322+matv864@users.noreply.github.com>
2021-11-19 15:30:35 +1000

    Update 2nd_JS.js

--- javascript/2nd_JS.js
+++ javascript/2nd_JS.js
@@ -26,3 +26,16 @@ block3.style.width = '1000px'
 block3.style.height = '200px'
 
 document.querySelector('main').append(block3)
+//
+let block4 = document.createElement('header')
+
+block4.style.backgroundColor = 'yellow'
+block4.innerHTML = "<img src='../images/pexels-erik-mclean-5558236.jpg' widht=100px height=100px>"
+block4.style.alignContent
+block4.style.width = '1000px'
+block4.style.height = '100px'
+//block4.innerText = "Вот что могу"
+
+document.querySelector('body').append(block4)
+//
+let text1 = document.getElementById('example1')
