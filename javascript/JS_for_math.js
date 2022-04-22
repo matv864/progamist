a8c278958d64cf5e814eefdd03a2a8043d35d693
matv864 <91239322+matv864@users.noreply.github.com>
2022-04-22 10:00:48 +1000

    Update JS_for_math.js

--- javascript/JS_for_math.js
+++ javascript/JS_for_math.js
@@ -1,4 +1,32 @@
-1. Уплачивать законно установленные налоги. 
-2. Вставать на учет в налоговых органах, если такая обязанность предусмотрена НК РФ. 
-3. Вести учет в установленном порядке своих доходов (расходов) и объектов налогообложения, 
-если такая обязанность предусмотрена законодательством о налогах и сборах.
+function zoro(){
+  a1 = document.myform.first.value
+  a2 = document.myform.second.value
+  a3 = document.myform.third.value
+  a4 = document.myform.fourth.value
+  a5 = document.myform.fifth.value
+  a6 = document.myform.sixth.value
+  a7 = document.myform.seventh.value
+  equation = a1 + "<h3>x<sup>5</sup></h3>" + "+" +
+    a2 + "<h3>x<sup>4</sup></h3>" + "+" +
+    a3 + "<h3>x<sup>3</sup></h3>" + "+" +
+    a4 + "<h3>x<sup>2</sup></h3>" + "+" +
+    a5 + "<h3>x</h3>" + "+" +
+    a6 + "=" + a7 + "<h1></h1>"
+
+  document.getElementById('result').innerHTML = ''
+  document.getElementById('myform').hidden = true
+  document.getElementById('button').hidden = true
+
+  document.getElementById('question').hidden = false
+  document.getElementById('equation').innerHTML=equation
+  document.getElementById('but').hidden = false
+}
+
+function nami(){
+  document.getElementById('question').hidden = true
+  //document.getElementById('equation').innerHTML=""
+  document.getElementById('but').hidden = true
+
+  document.getElementById('result').hidden = false
+  document.getElementById('result_equation').innerHTML= "твоё решение"
+}
